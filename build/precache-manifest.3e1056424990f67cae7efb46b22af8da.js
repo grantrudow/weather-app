6a1be03ed974d4c2b2802bbcce13862e21771fea
Grant Rudow <webdev.grant@gmail.com>
2020-09-17 20:40:14 -0500

    ci netlify build

---
+++ build/precache-manifest.3e1056424990f67cae7efb46b22af8da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7073bf28f99acf108516539391309c61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b549bdeaf81b60a68239",
+    "url": "/static/css/main.5d7411f3.chunk.css"
+  },
+  {
+    "revision": "f330d0dc399fd170ea35",
+    "url": "/static/js/2.97698615.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.97698615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b549bdeaf81b60a68239",
+    "url": "/static/js/main.0b5c4158.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "d48f7960e3fc2370b16a6a2d757347b3",
+    "url": "/static/media/cloudyDay.d48f7960.jpg"
+  },
+  {
+    "revision": "b639bf4016195a00d82f19294151082b",
+    "url": "/static/media/rainyDay.b639bf40.jpg"
+  },
+  {
+    "revision": "9a95acb200e0762e6e97c764c35ff23f",
+    "url": "/static/media/sunnyDay.9a95acb2.jpg"
+  },
+  {
+    "revision": "7a0bf9ecc84cb074deac5e2ab7002cd9",
+    "url": "/static/media/sunset.7a0bf9ec.jpg"
+  }
+]);
